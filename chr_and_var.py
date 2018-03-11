9a80ad3d0631e893a26036d1017f20bf852ee5fc
guijiezheng1119@gmail.com <zhengguijie1119@outlook.com>
2018-03-11 18:32:03 +0800

    add chr_and_var

---
+++ chr_and_var.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+#print('n =', n\n, 'f =', f\n, 's1 =',s1\n, 's2 =',s2)
+print( 'n = ', n,'\n')
